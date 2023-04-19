bd113f004870bd1bd755db073cc7ec0baa00b332
Art-gf <brotherhood.nod@rambler.ru>
2023-04-19 06:54:48 +0500

    up

--- remover/core.py
+++ remover/core.py
@@ -1,48 +1,50 @@
+from typing import List
 from PIL import Image
 import os
 from concurrent.futures import ThreadPoolExecutor
 
 F_IMPORT = "input"
 F_EXPORT = "output"
-HEIGHT_PX = 14
-COLOR_PX = [
+
+PX_HEIGHT = 14
+PX_COLOR = [
     [252, 195, 51],
     [254, 154, 9]
 ]
-POINTS_PX = 4 
+PX_POINTS = 4
 
 
-def is_comp(rgb: tuple):
+def is_comp(rgb: tuple) -> bool:
     for i in range(2):
-        res = [0.9 < rgb[i]/color[i] < 1.1 for color in COLOR_PX]
+        res = [0.9 < rgb[i]/color[i] < 1.1 for color in PX_COLOR]
         if not any(res):
-            return 
+            return False
     return True
 
-def get_points():
-    step = int(100/(POINTS_PX+1))
-    pix = [i/100 for i in range(step, 100, step)]
-    return pix[:POINTS_PX]
 
-def img_proc(name: str):
+def get_points() -> List[float]:
+    step = int(100/(PX_POINTS+1))
+    pix = [i/100 for i in range(step, 100, step)]
+    return pix[:PX_POINTS]
+
+
+def img_proc(name: str) -> None:
     path = f"{F_IMPORT}/{name}"
     dest = f"{F_EXPORT}/{name}"
     with Image.open(path) as img:
         img_rgb = img.convert("RGB")
-        w, h = img.size
         pix = get_points()
+        h = img.height-int(PX_HEIGHT/2)
         for perc in pix:
-            rgb = img_rgb.getpixel((int(w*perc), h-int(HEIGHT_PX/2)))
+            rgb = img_rgb.getpixel((int(img.width*perc), h))
             if not is_comp(rgb):
                 return
-        img = img.crop([0,0,w,h-14])
+        img = img.crop([0, 0, img.width, img.height-14])
         img.save(dest)
-    
-    
+
+
 def start():
     if files := os.listdir(F_IMPORT):
         with ThreadPoolExecutor() as w:
             for file in files:
                 w.submit(img_proc, file)
-    
-
